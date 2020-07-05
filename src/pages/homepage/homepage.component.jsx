4d5e67938e0a042eea589f78b7114239875c0d54
itdboy <itdboy@gmail.com>
2020-07-05 15:01:29 +0700

    update menu-items

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './homepage.styles.scss';
 import Directory from '../../components/directory/directory.component';
+import { useHistory } from 'react-router-dom';
 
-const HomePage = () =>(
+const HomePage = ({history}) =>(
     <div className='homepage'>
-          <Directory />
+          <Directory history={history} />
     </div>
 );
 
