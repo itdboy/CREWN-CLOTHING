d71e0a445d0ee8fe354553b294744472f4d729d9
itdboy <itdboy@gmail.com>
2020-07-14 00:26:36 +0700

    implemted firebase utils, include

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import './sign-in-and-sing-up.style.scss'
+import './sign-in-and-sign-up.style.scss'
 import SignIn from '../../components/sign-in/sign-in.component'
 import SignUp from '../../components/sing-up/sign-up.component'
 
 const SignInAndSignUpPage = () => (
-  <div className='sign-in-sing-up'>
+  <div className='sign-in-and-sign-up'>
     <SignIn />
     <SignUp />
   </div>
