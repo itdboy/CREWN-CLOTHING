49a73e4339d27a263f4a49a4c49b6d505dc31910
itdboy <itdboy@gmail.com>
2020-07-29 01:31:03 +0700

    update

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,16 +1,25 @@
 import React from 'react' ;
 import {connect} from 'react-redux';
 import CollectionItem from '../../components/collection-item/collection-item.component'
-
-import './collection.styles.scss'
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
+import './collection.styles.scss'
 
-const CollectionPage = ({match}) => {
-    console.log(match.params.collectionId)
+
+const CollectionPage = ({collection}) => {
+    const {title,items} = collection
+ 
     return (
     <div  className='collection-page'>
-        <h2>collection PAGE</h2>
+        <h2  className='title'>{title}</h2>
+        <div className='items'>
+
+            {
+                items.map(item => (
+                    <CollectionItem key={item.id} item={item} />
+                ))
+            }
+        </div>
     </div>
     ) 
     }
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,12 +1,6 @@
 import {createSelector} from 'reselect'
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneckers: 2,
-    jackets: 4,
-    womens: 4,
-    mens: 5
-}
+
 
 const selectShop = state => state.shop;
 
@@ -18,7 +12,5 @@ export const selectCollections = createSelector(
 export const selectCollection = collectionUrlParam => 
 createSelector(
     [selectCollections],
-    collections => collections.find(
-        collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    collections => collections[collectionUrlParam]
     )
-)
