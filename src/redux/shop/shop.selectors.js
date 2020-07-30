d9bdb7490f5087db894aada0fff22cb94d551465
itdboy <itdboy@gmail.com>
2020-07-30 23:32:30 +0700

    removing redux-logger

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -9,6 +9,14 @@ export const selectCollections = createSelector(
     shop=> shop.collections
 )
 
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections] , 
+    collections => Object.keys(collections).map(key => collections[key]) 
+)
+
+
+
 export const selectCollection = collectionUrlParam => 
 createSelector(
     [selectCollections],
